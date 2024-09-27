fd0baf4b0477459c9d1bda7370196f5792911c9c
Iffat15 <iffatkhanin@gmail.com>
2024-09-27 09:15:58 +0530

    Implement borrowBook method with availability check

--- tests/library.test.js
+++ tests/library.test.js
@@ -2,23 +2,27 @@ const Library = require('../src/Library');
 const Book = require('../src/Book');
 
 describe('Library Management System', () => {
-  let library;
-  let book;
+    let library;
+    let book;
 
-  beforeEach(() => {
-    library = new Library();
-    book = new Book('123456789', 'Clean Code', 'Robert C. Martin', 2008);
-  });
+    beforeEach(() => {
+        library = new Library();
+        book = new Book('123456789', 'Clean Code', 'Robert C. Martin', 2008);
+    });
 
-  test('should add a book to the library', () => {
-    library.addBook(book);
-    expect(library.viewAvailableBooks().length).toBe(1);
-    expect(library.viewAvailableBooks()[0].title).toBe('Clean Code');
-  });
+    test('should add a book to the library', () => {
+        library.addBook(book);
+        expect(library.viewAvailableBooks().length).toBe(1);
+        expect(library.viewAvailableBooks()[0].title).toBe('Clean Code');
+    });
     test('should allow borrowing an available book', () => {
-    library.addBook(book);
-    expect(library.borrowBook('123456789')).toBe(true);
-    expect(library.viewAvailableBooks().length).toBe(0);
-  });
+        library.addBook(book);
+        expect(library.borrowBook('123456789')).toBe(true);
+        expect(library.viewAvailableBooks().length).toBe(0);
+    });
+    test('should not allow borrowing a non-existent book', () => {
+        expect(() => library.borrowBook('999999999')).toThrow('Book not found or unavailable');
+    });
+
 
 });
